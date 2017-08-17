5c7f8fab031aff790b13aeeea8a73cb093805995
Isabel Palomar <isabel.palomarb@gmail.com>
2017-08-17 00:58:19 -0700

    4 Classes and functions

--- HelloKotlin/app/src/main/java/com/isabelpalomar/hellokotlin/MainActivity.kt
+++ HelloKotlin/app/src/main/java/com/isabelpalomar/hellokotlin/MainActivity.kt
@@ -2,7 +2,9 @@ package com.isabelpalomar.hellokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvMessage.text = "Hello Kotlin!"
+
+        toast("Hello")
+        niceToast("Kotlin")
+
+    }
+
+    fun niceToast(message: String,
+                  tag: String = MainActivity::class.java.simpleName,
+                  length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, "[$tag] $message", length).show()
     }
 }
